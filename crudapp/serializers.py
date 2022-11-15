032a6c1cb0dcc5d2c043b6a4e8c37ffc20cb7ef8
AdersonNazario <adnazasilva@outlook.com>
2022-11-15 11:10:51 -0300

    alterações formato retorno json

--- crudapp/serializers.py
+++ crudapp/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Contact
 
 class ContactSerializer(serializers.ModelSerializer):
+    servico = serializers.ReadOnlyField(source='servico.servico_text')
     class Meta:
         model = Contact
         fields = ['nome', 'telefone', 'servico', 'agendamento', 'hora', 'author']
